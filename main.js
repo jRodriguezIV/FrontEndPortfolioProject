773caf5b98d603d6f1156b8458fdd07c481ec30b
jRodriguezIV <j.a.rodrigueziv@gmail.com>
2023-01-26 14:08:49 -0500

    added second html css and js files with functionality for a Warnings Page

--- main.js
+++ main.js
@@ -3,11 +3,11 @@ const URL = "https://api.fda.gov"
 let URLafter= ""
 const KEY = ".json?api_key=bKom7JyWySs5BqcEmqXhdlkPJsokfGFeRF5at5r1"
 const SEARCH = "&search="
-const data1 = fetch(`${URL}/drug/label.json?limit=1`)
-const data2 = fetch(`${URL}/drug/event.json?limit=1`)
 let disc = document.getElementsByClassName("disc")
 let re = document.getElementById("results")
 let link = document.getElementById("link")
+let val = ""
+let body2 = document.getElementById("body2")
 
 
 let form = document.querySelector("form")
@@ -26,30 +26,30 @@ fetch (
         disc[0].append(p)
         p.textContent = result.meta.disclaimer
         // console.log(disc[0])
-
-    })
+        
+    }).catch(error=>console.log(error))
     // console.log(form)
-
+    
     form.addEventListener("submit", (event)=> {
         
         event.preventDefault();
+        val = input.value
+        localStorage.setItem("val", input.value)
         // console.log(input.value)
         
         fetch (
-
+            
             `${URL}/drug/drugsfda${KEY}${SEARCH}${input.value.split(" ").join("+")}&limit=20`
-            ).then((response)=> {
-                return response.json()
-            })
+            ).then((response)=> response.json())
             .then((result)=>{
-               
                 let a = document.querySelector("a")
                 let p = document.createElement("p")
                 a ? a.remove() : console.log("nothing")
-                p.innerHTML = `<strong><a id=link href="#">Events</a></strong>`
+                p.innerHTML = `<strong><a id="link" href="javascript:void(0)" onClick="events()">Warnings</a></strong>`
+                // <a id="link" href="javascript:null" onClick="javascript:events()">Events</a>
                 form.after(p)
-
-
+                
+                
                 re.innerHTML = ""
 
                
@@ -72,57 +72,75 @@ fetch (
                     <span><b>Product Type:</b><br>${el.openfda.product_type}</span>
                     <span><b>Route:</b><br>${el.openfda.route}</span>
                     `
+
                     }}
                 })
+                
+                
+            }).catch((err) => console.log(err))
+            //
+        })
+        
+        
+        function events(e) {
+            window.location.href = "./index2.html"
+            
+            
+            // fetch (
+                //     `${URL}/drug/event${KEY}${SEARCH}${input.value.split(" ").join("+")}&limit=20`
+                // ).then(response => response.json()
+                // ).then(result => {
+                    //     console.log(result)
+                    
+                    
+                    // }).catch(error=>console.log(error))
+                    
+                    
+                    //  window.history.replaceState("/index2.html" )
+                    
+                    
+                }
+                
+                
+                
+                
+                
+                
 
-
-
-            }).catch((err) => {
-
-                console.log(err)
-            })
-        //
-    })
-
-
-
-
-
-
-
-        //  API LINK : https://open.fda.gov/apis/anatomy-of-a-response/
-        // going to create a drop down after search is made, labeled with Searchable Fields to relay information of choice.
-        // will also add a list of "important" fields at default after search.
-        // will brainstorm on how to create a list of all the current drugs labels.
-        // will brainstorm on another button(random or drop) for those who can't think of a drug to search.
-        // product_type, generic_name, brand_name, substance_name, is_origina_packager,
-
-
-// Promise.all([data1, data2])
-//     .then((response) => response.forEach((res) => {
-
-//         promise(res.json())
-
-//     }))
-//     .catch((error=>console.log(error)))
-
-//     let promise = (prom) => {
-//         prom.then()
-//     }
-
- 
-
-
-
-
-
-
-// fetch(
-//     `${URL}/drug.json?limit=1`
-// ).then((response) => response.json())
-//     .then((result) => {
-
-
-
-//         console.log(result.results[0])
-//     }).catch((err) => console.log(err))
+    
+    //  API LINK : https://open.fda.gov/apis/anatomy-of-a-response/
+    // going to create a drop down after search is made, labeled with Searchable Fields to relay information of choice.
+    // will also add a list of "important" fields at default after search.
+    // will brainstorm on how to create a list of all the current drugs labels.
+    // will brainstorm on another button(random or drop) for those who can't think of a drug to search.
+    // product_type, generic_name, brand_name, substance_name, is_origina_packager,
+    
+    
+    // Promise.all([data1, data2])
+    //     .then((response) => response.forEach((res) => {
+        
+        //         promise(res.json())
+        
+        //     }))
+        //     .catch((error=>console.log(error)))
+        
+        //     let promise = (prom) => {
+            //         prom.then()
+            //     }
+            
+            
+            
+            
+            
+            
+            
+            
+            // fetch(
+                //     `${URL}/drug.json?limit=1`
+                // ).then((response) => response.json())
+                //     .then((result) => {
+                    
+                    
+                    
+                    //         console.log(result.results[0])
+                    //     }).catch((err) => console.log(err))
